fe016bfa389320f95e3d0f0db455ffdafcb5da46
Masamichi Hosoda <trueroad@trueroad.jp>
2017-04-19 23:26:38 +0900

    Add emacs-w32-ime
    without IME patches

---
+++ emacs-w32-ime/postinstall.sh
@@ -0,0 +1,7 @@
+PV=25.1
+
+/usr/sbin/update-alternatives \
+    --install /usr/bin/emacs emacs /usr/emacs-w32-ime/${PV}/bin/emacs.exe 40
+/usr/sbin/update-alternatives \
+    --install /usr/bin/emacsclient emacsclient /usr/emacs-w32-ime/${PV}/bin/emacsclient.exe 40
+ln -s /usr/share/emacs/site-lisp /usr/emacs-w32-ime/${PV}/share/emacs/site-lisp/emacs-site-lisp
---
+++ emacs-w32-ime/preremove.sh
@@ -0,0 +1,7 @@
+PV=25.1
+
+/usr/sbin/update-alternatives \
+    --remove emacs /usr/emacs-w32-ime/${PV}/bin/emacs.exe
+/usr/sbin/update-alternatives \
+    --remove emacsclient /usr/emacs-w32-ime/${PV}/bin/emacsclient.exe
+rm /usr/emacs-w32-ime/${PV}/share/emacs/site-lisp/emacs-site-lisp
