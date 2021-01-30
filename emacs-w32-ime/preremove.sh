688907902f7d3202568a485e909799212c382dfd
Masamichi Hosoda <trueroad@trueroad.jp>
2021-01-30 11:09:45 +0900

    Remove emacs-w32-ime
    Use tr-ime.
    https://melpa.org/#/tr-ime
    https://github.com/trueroad/tr-emacs-ime-module

--- emacs-w32-ime/preremove.sh
+++
@@ -1,7 +0,0 @@
-PV=26.3
-
-/usr/sbin/update-alternatives \
-    --remove emacs /usr/emacs-w32-ime/${PV}/bin/emacs.exe
-/usr/sbin/update-alternatives \
-    --remove emacsclient /usr/emacs-w32-ime/${PV}/bin/emacsclient.exe
-rm /usr/emacs-w32-ime/${PV}/share/emacs/site-lisp/emacs-site-lisp
